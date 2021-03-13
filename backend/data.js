c4af6174f838f4a17a10e8b301821a1ea702e895
Khaled Khalil <khaled.m.hassan.94@gmail.com>
2021-03-13 16:36:09 +0200

    async handler, mongoose, connect to MongoDB, created 2 users

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,19 @@
+import bcrypt from "bcryptjs";
 const data = {
+  users: [
+    {
+      name: "Khaled",
+      email: "admin@example.com",
+      password: bcrypt.hashSync("1234", 8),
+      isAdmin: true,
+    },
+    {
+      name: "Hossam",
+      email: "user@example.com",
+      password: bcrypt.hashSync("1234", 8),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       _id: "1",
