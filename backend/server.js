dd6a06bf876c264f9607ce5b8bc6f99b5beec5f9
Khaled Khalil <khaled.m.hassan.94@gmail.com>
2021-03-13 17:01:45 +0200

    dynamic products (loaded from MongoDB)

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-import data from "./data.js";
+import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
 
 const app = express();
@@ -10,25 +10,14 @@ mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {
   useCreateIndex: true,
 });
 
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
-
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
 app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 
 app.use((err, req, res, next) => {
-  res.status(5000).send({ message: err.message });
+  res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
